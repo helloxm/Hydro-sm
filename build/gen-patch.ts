119d4c17396344e4da15661ee48a581530635326
helloxm <fjt0327@163.com>
2025-01-23 14:23:15 +0800

    修改上层参数

--- build/gen-patch.ts
+++ build/gen-patch.ts
@@ -1,21 +1,23 @@
-import child from 'child_process';
-import fs from 'fs';
-import path from 'path';
-import superagent from 'superagent';
+import child from "child_process";
+import fs from "fs";
+import path from "path";
+import superagent from "superagent";
 
-if (!process.argv[2]) throw new Error('No target specified');
+if (!process.argv[2]) throw new Error("No target specified");
 
 // TODO support module other than packages
-const target = process.argv[2].startsWith('packages/') ? process.argv[2] : `packages/${process.argv[2]}`;
+const target = process.argv[2].startsWith("packages/")
+  ? process.argv[2]
+  : `packages/${process.argv[2]}`;
 
 const result = child.execSync(`git diff ${target}`);
-const patch = result.toString().replace(new RegExp(`${target}/`, 'g'), '');
+const patch = result.toString().replace(new RegExp(`${target}/`, "g"), "");
 const filename = `${path.basename(target)}.patch`;
 fs.writeFileSync(filename, patch);
-superagent.post('https://hydro.ac/paste')
-    .set('accept', 'application/json')
-    .send({ body: patch, filename })
-    .end((err, res) => {
-        if (err) throw err;
-        console.log('Paste created on ', res.text);
-    });
+// superagent.post('https://hydro.ac/paste')
+//     .set('accept', 'application/json')
+//     .send({ body: patch, filename })
+//     .end((err, res) => {
+//         if (err) throw err;
+//         console.log('Paste created on ', res.text);
+//     });
