484b9ec544c5b27ce01580d22cd99e418a059aa2
helloxm <fjt0327@163.com>
2025-12-02 15:13:42 +0800

    udpate new  Hydro v5.0.0-beta.13

--- addon/install.sh
+++ addon/install.sh
@@ -6,6 +6,7 @@ cp ../packages/hydrooj/src/model/domain.ts /usr/local/share/.config/yarn/global/
 cp ../packages/hydrooj/src/model/contest.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/model/contest.ts -rf 
 cp ../packages/hydrooj/src/model/setting.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/model/setting.ts -rf 
 cp ../packages/hydrooj/src/model/builtin.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/model/builtin.ts -rf 
+cp ../packages/common/permission.ts /usr/local/share/.config/yarn/global/node_modules/@hydrooj/common/permission.ts -rf
 cp ../packages/hydrooj/src/handler/domain.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/handler/domain.ts -rf 
 cp ../packages/hydrooj/src/handler/home.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/handler/home.ts -rf 
 cp ../packages/hydrooj/src/handler/user.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/handler/user.ts -rf 
@@ -14,8 +15,8 @@ cp ../packages/hydrooj/src/handler/homework.ts /usr/local/share/.config/yarn/glo
 cp ../packages/hydrooj/src/handler/contest.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/handler/contest.ts -rf 
 cp ../packages/hydrooj/src/handler/problem.ts /usr/local/share/.config/yarn/global/node_modules/hydrooj/src/handler/problem.ts -rf 
 cp ../packages/hydrooj/setting.yaml /usr/local/share/.config/yarn/global/node_modules/hydrooj/setting.yaml -rf
-cp templates/* /root/addon/templates/ -rf
-cp locales/* /root/addon/locales/ -rf 
+# cp templates/* /root/addon/templates/ -rf
+# cp locales/* /root/addon/locales/ -rf 
 # cp public/* /root/addon/public/ -rf 
 
 pm2 restart hydrooj
