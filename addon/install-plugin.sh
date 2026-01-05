429d142af80d32f0ec821f5fafbb7f37874378d9
root <root@fang>
2026-01-05 21:44:35 +0800

    add install-plugin.sh

---
+++ addon/install-plugin.sh
@@ -0,0 +1,8 @@
+
+rm -rf /root/.hydro/addons/buydomain && cp ../packages/buydomain  /root/.hydro/addons/ -rf && hydrooj addon add /root/.hydro/addons/buydomain
+rm -rf /root/.hydro/addons/smpaste && cp ../packages/smpaste  /root/.hydro/addons/ -rf && hydrooj addon add /root/.hydro/addons/smpaste
+rm -rf /root/.hydro/addons/smsuggestion && cp ../packages/smsuggestion  /root/.hydro/addons/ -rf add hydrooj addon add /root/.hydro/addons/smsuggestion
+rm -rf /root/.hydro/addons/slidev && cp ../packages/slidev  /root/.hydro/addons/ -rf && hydrooj addon add /root/.hydro/addons/slidev
+rm -rf /root/.hydro/addons/addon && cp ../addon  /root/.hydro/addons/ -rf && hydrooj addon add /root/.hydro/addons/addon
+
+pm2 restart hydrooj --update-env
